b40bfa62125c1ff6354bcb5e28cb71aec65627bb
Jackie <jackieli1479@gmail.com>
2025-10-27 17:30:01 -0400

    Payment summary - Checkout Cart
    Once the user has logged in, added items to cart and clicks checkout, user must fill in credentials then click payout to see their summary.

--- BookStoreGUI/MainWindow.xaml.cs
+++ BookStoreGUI/MainWindow.xaml.cs
@@ -193,7 +193,7 @@ namespace BookStoreGUI
                 return;
             }
             // When i want to pass the cart contents to the payment window so it can show a summary i will add cart.shoppingCart in the parantheses
-            var pay = new PaymentWindow();
+            var pay = new PaymentWindow(cart.shoppingCart);
             pay.ShowDialog();
         } 
 
