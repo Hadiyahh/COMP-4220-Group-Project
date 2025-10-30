5ca632089f5563950bb0f508bcd43bdb2bfbdc80
Hadiyah Arif <arifh@uwindsor.ca>
2025-10-30 00:31:04 -0400

    <Test>/added the test cases for the payment fom

---
+++ BookStoreLIB/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BookStoreGUI
+{
+    public partial class Payment
+    {
+        public static (bool, string) Validate(string name, string number, string expiry, string cvv, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name) ||
+               string.IsNullOrWhiteSpace(number) ||
+               string.IsNullOrWhiteSpace(expiry) ||
+               string.IsNullOrWhiteSpace(cvv) ||
+               string.IsNullOrWhiteSpace(email))
+                return (false, "Please fill in all fields.");
+
+            if (cvv.Length != 3 || number.Length != 16 || !expiry.Contains("/"))
+                return (false, "Please check card details.");
+
+            try { _ = new MailAddress(email); }
+            catch { return (false, "Invalid email."); }
+
+            return (true, string.Empty);
+        }
+    }
+}
